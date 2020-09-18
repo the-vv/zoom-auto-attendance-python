e2a2f0090bb3293ebdd9f9b86a692b29cf7ff5a8
vv-2001 <vishnuvinod2772001@gmail.com>
2020-09-18 12:02:35 +0530

    stable version public

--- Auto Attandance.py
+++ Auto Attandance.py
@@ -5,14 +5,20 @@ except ModuleNotFoundError:
     print('Unable to start, Pls install pyautogui module, then restart')
     x = input('press enter to exit')
     sys.exit()
+number = pyautogui.prompt(text='Enter your mesage to send', title='Enter your message' , default='')
+if not number:    
+    if pyautogui.confirm(text='Number not enetered', title='What do you want to do?', buttons=['Continue', 'Exit']) != 'Continue': 
+        print('Exiting')
+        sys.exit()
 print('Zoom auto attendance Bot\nStarted...\nPress Ctrl + c to stop')
+print('Your Number: ', number)
 print('-------------------------')
-while not pyautogui.locateOnScreen('images/chatbox.png'):
-    print('Chatbox not found')
-print('chatbox found')
-positions = []
-while 1:
-    try:
+try:
+    while not pyautogui.locateOnScreen('images/chatbox.png'):
+        print('Chatbox not found')
+    print('chatbox found')
+    positions = []
+    while 1:
         position = pyautogui.locateAllOnScreen('images/from.png') 
         for pos in position: 
             if pos.top not in positions:
@@ -26,16 +32,16 @@ while 1:
                 except TypeError:
                     print('Error finding message box!!!')
                     continue
-                pyautogui.write('B 45', interval=0.10)
+                pyautogui.write(number, interval=0.10)
                 pyautogui.press('enter')
                 x = input('Operation Success, press enter to exit')
                 sys.exit()
         else:
             print('Waiting for annandance to start...')
-    except pyautogui.ImageNotFoundException:
-        print('Not Found on screen')
-    except KeyboardInterrupt:
-        print('Exiting')
-        sys.exit()
+except pyautogui.ImageNotFoundException:
+    print('Not Found on screen')
+except KeyboardInterrupt:
+    print('Exiting')
+    sys.exit()
 
         
