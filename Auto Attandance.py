916400c7bd9e90c52130655e6b4dc3c80a1bedc2
vv-2001 <vishnuvinod2772001@gmail.com>
2020-09-18 10:24:17 +0530

    added minimum 3 attandance to enter attandance

--- Auto Attandance.py
+++ Auto Attandance.py
@@ -1,20 +1,33 @@
 import pyautogui
-import os
+import sys
 print('Zoom auto attendance Bot\nStarted...\nPress Ctrl + c to stop')
 print('-------------------------')
+while not pyautogui.locateOnScreen('images/chatbox.png'):
+    print('Chatbox not found')
+print('chatbox found')
+positions = []
 while 1:
     try:
-        if pyautogui.locateOnScreen('images/from.png'):
-            print('Attandance started, Entering attandence...')
-            pyautogui.click('images/chat.png')
-            pyautogui.write('B 45', interval=0.10)
-            pyautogui.press('enter')
-            x = input('Operation Success, press enter to exit')
-            os.exit()
+        position = pyautogui.locateOnScreen('images/from.png')
+        position = pyautogui.center(position)[1]
+        if position:
+            if position not in positions:
+                positions.append(position)
+                position = None
+            print(len(positions), 'Attandances found')
+            if len(positions) >= 3 :
+                print('Attandance started, Entering attandence...')
+                pyautogui.click('images/chat.png')
+                pyautogui.write('B 45', interval=0.10)
+                pyautogui.press('enter')
+                x = input('Operation Success, press enter to exit')
+                sys.exit()
         else:
-            print('Checking for attandance...')
+            print('Waiting for annandance to start...')
+    except pyautogui.ImageNotFoundException:
+        print('NotFound')
     except KeyboardInterrupt:
         print('Exiting')
-        os.exit()
+        sys.exit()
 
         
